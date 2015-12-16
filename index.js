ArtskydJ/disable-oom-killer

commit 40b0c060d7a64abcfb4e8b01b17a25f258ea0832

Don't throw on fs read error

test.js
@@ -3,13 +3,28 @@ var fs = require('fs')
 var spawn = require('child_process').spawn
 var disableOomKiller = require('./index.js')
 
+function expect(t, pid, oom_adj, oom_score_adj) {
+	try {
+		var content1 = fs.readFileSync('/proc/' + pid + '/oom_adj', 'utf8')
+		t.equal(content1, oom_adj)
+	} catch (err) {
+		t.ifError(err)
+	}
+	
+	try {
+		var content2 = fs.readFileSync('/proc/' + pid + '/oom_score_adj', 'utf8')
+		t.equal(content2, oom_score_adj)
+	} catch (err) {
+		t.ifError(err)
+	}
+}
+
 if (process.platform === 'linux') {
 	test('async defaults', function (t) {
 		disableOomKiller(function (err) {
 			t.ifError(err)
 
-			t.equal(fs.readFileSync('/proc/' + process.pid + '/oom_adj', 'utf8'), '-17\n')
-			t.equal(fs.readFileSync('/proc/' + process.pid + '/oom_score_adj', 'utf8'), '-1000\n')
+			expect(t, process.pid, '-17\n', '-1000\n')
 
 			t.end()
 		})
@@ -24,8 +39,7 @@ if (process.platform === 'linux') {
 		}, function (err) {
 			t.ifError(err)
 
-			t.equal(fs.readFileSync('/proc/' + proc.pid + '/oom_adj', 'utf8'), '-16\n')
-			t.equal(fs.readFileSync('/proc/' + proc.pid + '/oom_score_adj', 'utf8'), '-999\n')
+			expect(t, process.pid, '-16\n', '-999\n')
 
 			t.end()
 		})
@@ -34,22 +48,22 @@ if (process.platform === 'linux') {
 	test('sync defaults', function (t) {
 		t.doesNotThrow(disableOomKiller.sync)
 
-		t.equal(fs.readFileSync('/proc/' + process.pid + '/oom_adj', 'utf8'), '-17\n')
-		t.equal(fs.readFileSync('/proc/' + process.pid + '/oom_score_adj', 'utf8'), '-1000\n')
+		expect(t, process.pid, '-17\n', '-1000\n')
 
 		t.end()
 	})
 
 	test('sync options', function (t) {
 		var proc = spawn('ls', [ '-1' ])
-		disableOomKiller.sync({
-			pid: proc.pid,
-			oom_adj: 0,
-			oom_score_adj: 0
+		t.doesNotThrow(function () {
+			disableOomKiller.sync({
+				pid: proc.pid,
+				oom_adj: 0,
+				oom_score_adj: 0
+			})
 		})
 
-		t.equal(fs.readFileSync('/proc/' + proc.pid + '/oom_adj', 'utf8'), '0\n')
-		t.equal(fs.readFileSync('/proc/' + proc.pid + '/oom_score_adj', 'utf8'), '0\n')
+		expect(t, process.pid, '0\n', '0\n')
 
 		t.end()
 	})

commit 20fd46b2be01ef0ada31c4781aa2e9c6c48b441b

Fix what I broke in 40b0c06
I copy/pasted process.pid, and forgot to replace instances of proc.pid

index.js
@@ -3,13 +3,11 @@ var fs = require('fs')
 var xtend  = require('xtend')
 
 function parseOpts(options) {
-	var o = xtend({
+	return xtend({
 		pid: process.pid,
 		oom_score_adj: '-1000',
 		oom_adj: '-17'
 	}, options)
-	console.log(o.oom_adj, o.oom_score_adj)
-	return o
 }
 
 module.exports = function disableOomKiller(options, cb) {

test.js
@@ -4,28 +4,25 @@ var spawn = require('child_process').spawn
 var disableOomKiller = require('./index.js')
 
 function expect(t, pid, oom_adj, oom_score_adj) {
-	setTimeout(function () {
-		try {
-			t.equal(fs.readFileSync('/proc/' + pid + '/oom_adj', 'utf8'), oom_adj)
-		} catch (err) {
-			t.ifError(err)
-		}
-		
-		try {
-			t.equal(fs.readFileSync('/proc/' + pid + '/oom_score_adj', 'utf8'), oom_score_adj)
-		} catch (err) {
-			t.ifError(err)
-		}
+	try {
+		t.equal(fs.readFileSync('/proc/' + pid + '/oom_adj', 'utf8'), oom_adj)
+	} catch (err) {
+		t.ifError(err)
+	}
+	
+	try {
+		t.equal(fs.readFileSync('/proc/' + pid + '/oom_score_adj', 'utf8'), oom_score_adj)
+	} catch (err) {
+		t.ifError(err)
+	}
 
-		t.end()
-	}, 100)
+	t.end()
 }
 
 if (process.platform === 'linux') {
 	test('async defaults', function (t) {
 		disableOomKiller(function (err) {
 			t.ifError(err)
-
 			expect(t, process.pid, '-17\n', '-1000\n')
 		})
 	})
@@ -38,14 +35,12 @@ if (process.platform === 'linux') {
 			oom_score_adj: -999
 		}, function (err) {
 			t.ifError(err)
-
-			expect(t, process.pid, '-16\n', '-999\n')
+			expect(t, proc.pid, '-16\n', '-999\n')
 		})
 	})
 
 	test('sync defaults', function (t) {
 		t.doesNotThrow(disableOomKiller.sync)
-
 		expect(t, process.pid, '-17\n', '-1000\n')
 	})
 
@@ -58,8 +53,7 @@ if (process.platform === 'linux') {
 				oom_score_adj: 0
 			})
 		})
-
-		expect(t, process.pid, '0\n', '0\n')
+		expect(t, proc.pid, '0\n', '0\n')
 	})
 	
 	test('child_process object as options', function (t) {